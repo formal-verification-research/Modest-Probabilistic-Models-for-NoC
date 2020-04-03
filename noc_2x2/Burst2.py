0f1c95b9b76f3b83939fdda2f3f55a84300ddc46
RileyLRoberts <rileylroberts.97@gmail.com>
2020-04-03 17:28:45 -0600

    added python files to run for different bursts

---
+++ noc_2x2/Burst2.py
@@ -0,0 +1,9 @@
+import subprocess
+M = 2 # on cycles
+N = 8 # off cycles
+
+output = ["Burst2Output0.txt", "Burst2Output1.txt", "Burst2Output2.txt", "Burst2Output3.txt", "Burst2Output4.txt", "Burst2Output5.txt", "Burst2Output6.txt", "Burst2Output7.txt", "Burst2Output8.txt"]
+
+for x in range (0,8):
+	subprocess.run(["/Users/Riley/Desktop/Modest/modest", "mcsta", "BurstyMode.modest", "-E", "DUR = 100, Mvar = %s, Nvar = %s, Startvar = %s" % (str(M), str(N), str(x)), "--unsafe", "-O", str(output[x])])
+
