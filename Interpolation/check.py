a467f7d71b2c25c1e445c5689f51da00f6dc47bd
benjaylew <benjaylew@el176-threadripper.usu.edu>
2020-09-14 06:58:19 -0600

    Automated generate results

--- Interpolation/check.py
+++ Interpolation/check.py
@@ -1,11 +1,16 @@
-import os
+import re
 
-for val0 in range (1,3):
-	for i in range (1,3):
-		val1 = (i + 1) % 4
-		for j in range (1,3):
-			val2 = (j + 2) % 4
-			for val3 int range (0,2):
-				outputFile = "output" + str(val0) + str(val1) + str(val2) + str(val3)
-				command = "mcsta arbiterTest13state.modest -E val0=" + str(val0) + " val1=" + str(val1) + " val2=" + str(val2) + " val3=" + str(val3) + " -O " + str(outputFile)
-				os.system(command)
+probabilityRegex = re.compile(r'(\d\.\d+)\n')
+
+file = open("testResults/output1000.txt")
+data = file.read()
+dataList = str(data).split("Probability: ")
+finalData = []
+
+for item in dataList:
+    match = probabilityRegex.search(item)
+    if match:
+        print(match)
+        finalData.append(match.group().split('\n')[0])
+
+print(finalData)
