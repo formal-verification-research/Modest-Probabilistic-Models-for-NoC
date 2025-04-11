21b1a85fac2b4cc44f40d1ab29695e8455175bb9
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-11 12:50:38 -0600

    finalized before sim

--- NoC_Modular/FMCAD25/tools/fmcad.py
+++ NoC_Modular/FMCAD25/tools/fmcad.py
@@ -97,12 +97,12 @@ def simulate(*, result_path: Path = Path("results"), size: int, type: NoiseType,
 
     return probs 
             
-def noc_2x2():
-    """ 2x2 simulations """
-    simulate(size=4, type=NoiseType.RESISTIVE, threshold=1, clk_upper=None, stride=1)
-    simulate(size=4, type=NoiseType.RESISTIVE, threshold=5, clk_upper=None, stride=2)
-    simulate(size=4, type=NoiseType.RESISTIVE, threshold=10, clk_upper=None, stride=5)
-    simulate(size=4, type=NoiseType.RESISTIVE, threshold=20, clk_upper=None, stride=10)
+def noc_2x2_resistive():
+    """ 2x2 resistive simulations """
+    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=1, clk_upper=None, stride=1)
+    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=5, clk_upper=None, stride=2)
+    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=10, clk_upper=None, stride=5)
+    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=20, clk_upper=None, stride=10)
 
 if __name__ == "__main__":
-    noc_2x2()
+    noc_2x2_resistive()
