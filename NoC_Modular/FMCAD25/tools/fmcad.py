4cbde590673fef1571cea0e44f2d140985f9d354
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-11 12:51:32 -0600

    fixed results path

--- NoC_Modular/FMCAD25/tools/fmcad.py
+++ NoC_Modular/FMCAD25/tools/fmcad.py
@@ -99,10 +99,10 @@ def simulate(*, result_path: Path = Path("results"), size: int, type: NoiseType,
             
 def noc_2x2_resistive():
     """ 2x2 resistive simulations """
-    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=1, clk_upper=None, stride=1)
-    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=5, clk_upper=None, stride=2)
-    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=10, clk_upper=None, stride=5)
-    simulate(size=4, result_path=Path("2x2/results"), type=NoiseType.RESISTIVE, threshold=20, clk_upper=None, stride=10)
+    simulate(size=4, result_path=Path("results/2x2"), type=NoiseType.RESISTIVE, threshold=1, clk_upper=None, stride=1)
+    simulate(size=4, result_path=Path("results/2x2"), type=NoiseType.RESISTIVE, threshold=5, clk_upper=None, stride=2)
+    simulate(size=4, result_path=Path("results/2x2"), type=NoiseType.RESISTIVE, threshold=10, clk_upper=None, stride=5)
+    simulate(size=4, result_path=Path("results/2x2"), type=NoiseType.RESISTIVE, threshold=20, clk_upper=None, stride=10)
 
 if __name__ == "__main__":
     noc_2x2_resistive()
