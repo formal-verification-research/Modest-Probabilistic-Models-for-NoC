03a68d9d12faf585208af6f76f9d21edcae5cacc
Nick Waddoups <nick.waddoups@usu.edu>
2025-11-11 23:28:24 -0700

    Removed all except Modular NoC work

--- noc_2x2/Burst5.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-M = 5 # on cycles
-N = 10 # off cycles
-
-output = ["Burst5Output0.txt", "Burst5Output1.txt", "Burst5Output2.txt", "Burst5Output3.txt", "Burst5Output4.txt", "Burst5Output5.txt", "Burst5Output6.txt", "Burst5Output7.txt", "Burst5Output8.txt", "Burst5Output9.txt", "Burst5Output10.txt"]
-
-for x in range (0,10):
-	subprocess.run(["~/ModestFiles/Modest/modest", "mcsta", "BurstyMode.modest", "-E", "DUR = 10000, Mvar = %s, Nvar = %s, Startvar = %s" % (str(M), str(N), str(x)), "--unsafe", "-O", str(output[x])])
